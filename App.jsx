520820383fd3c556d4f4643d3f655649ec992658
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-07-24 14:19:16 +0300

    Complete navbar

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App(){
+    return(
+        <div className="content">
+            <Navbar />
+            <div className="delimiter"></div> 
+        </div>
+    )
+}
+
+export default App;
