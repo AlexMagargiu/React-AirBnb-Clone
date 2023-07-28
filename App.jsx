2cc2a422a3aa290cfecf0bdb70ee99c82529b788
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-07-28 15:10:38 +0300

    Update hard coded code into better code

--- App.jsx
+++ App.jsx
@@ -8,7 +8,14 @@ function App(){
             <Navbar />
             <div className="delimiter"></div> 
             <Hero />
-            <Card />
+            <Card
+                img = "./images/katie-zaferes.png"
+                rating = "5.0"
+                reviewCount = {6}
+                country = "USA"
+                title = "Life Lessons with Katie Zaferes"
+                price = {136} 
+            />
         </div>
     )
 }
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,16 @@
-import person from "../images/katie-zaferes.png"
 import star from "../images/star.png"
 
-function Card(){
+function Card(props){
     return(
         <div className="card">
-            <img src={person} alt="An image of a person" className="person-img"></img>
+            <img src={props.img} alt="An image of a person" className="person-img"></img>
             <div className="information">
                 <div className="rating">
                     <img src={star}></img>
-                    <p>5.0 <span className="gray">(6) · USA</span></p>
+                    <p>{props.rating} <span className="gray">({props.reviewCount}) · {props.country}</span></p>
                 </div>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><strong>From $136 / </strong>person</p>
+                <p>{props.title}</p>
+                <p><strong>From ${props.price} / </strong>person</p>
             </div>
         </div>
         
